3d865f327c4100d20a759dc4e65313711ccf71a6
distribuidorarealacai <distribuidora.realacai@gmail.com>
2026-08-12 16:40:54 -0300

    Atualização ok

--- dash_flask_ofc.py
+++ dash_flask_ofc.py
@@ -624,4 +624,4 @@ def forcar_atualizacao():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print(f"Dashboard online rodando em http://0.0.0.0:{port}")
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False, threaded=True)
